a4de541456af327e6d33b873b355a1ba1f14e929
Beatrice Casey <bac041@bucknell.edu>
2020-06-25 14:50:09 -0400

    Fixed bug in Background Changer.

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -4,21 +4,33 @@ import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.debuggingchallenges.R;
 
 public class ChangeBackgroundActivity extends AppCompatActivity {
 
     private int oldColor = Color.BLUE;
+    Button gobtn;
+    View mainView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+
+        gobtn = findViewById(R.id.btnGo);
+        mainView = findViewById(android.R.id.content);
+        //Toast.makeText(getApplicationContext(), "app open", Toast.LENGTH_SHORT).show();
+        gobtn.setOnClickListener(OnClickInstance);
+
+
     }
 
     public void onGoClick(View view) {
-        View mainView = findViewById(android.R.id.content);
+
         mainView.setBackgroundColor(getNextColor());
     }
 
@@ -27,4 +39,12 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
         oldColor = newColor;
         return newColor;
     }
+
+    private View.OnClickListener OnClickInstance = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "button", Toast.LENGTH_SHORT).show();
+            onGoClick(mainView);
+        }
+    };
 }
